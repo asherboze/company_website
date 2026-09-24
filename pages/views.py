92ee3c273c616e02e2c2d4cb0d4bbb61ff7a5279
John Doe <asherboze123@gmail.com>
2026-09-23 21:44:12 -0600

    products page

--- pages/views.py
+++ pages/views.py
@@ -17,3 +17,14 @@ class AboutPageView(TemplateView):
         context["contact_address"] = "123 Main Street"
         context["phone_number"] = "555-555-5555"
         return context
+
+class ProductsPageView(TemplateView):
+    template_name = "products.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["product_1"] = "Sweat"
+        context["product_2"] = "Saliva"
+        context["product_3"] = "Keratin"
+        context["product_4"] = "Breath"
+        return context
